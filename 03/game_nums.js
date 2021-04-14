ea4bb837ac2084d8a08e471871ee29fc083b8862
pixelpusher-ka <ka.des@mail.ru>
2021-04-14 11:38:21 +1000

    Lesson03

--- 03/game_nums.js
+++ 03/game_nums.js
@@ -1,47 +1,60 @@
 window.onload = function() {
     var end_game = false;
-    var rnd_num, n, count;
+    var rnd_num, n, count_steps;
 
-    function generate_number(count) {
+    function generate_number(count = 4) {
         var n = [], rnd_n;
-        for(i = 0; i < count; i++) {
+        for(var i = 0; i < count; i++) {
             rnd_n = Math.floor(Math.random() * 10);
-            if( n.indexOf(rnd_n) < 0) n.push(rnd_n); else i--;
+            if(n.indexOf(rnd_n) < 0) n.push(rnd_n); else i--;
         }
+        console.log(n.join(""));
         return n.join("");
     }
-    console.log(generate_number(4));
 
-    //for(i = 0; i < 10; i++) console.log(generate_number(4));
+    function check_cows(n1, n2 = rnd_num) {
+        var result = [];
+        n1 += ""; n2 += "";
+        for(var i = 0; i < n1.length; i++) 
+            if(n2.indexOf(n1[i]) >= 0) result.push(n1[i]);
+        return result;
+    }
 
-    // while (!end_game) {
-    //     rnd_num = Math.floor(Math.random() * 8999 + 1000);
-    //     count = 0;
-    //     console.log(rnd_num);
-    //     n = prompt("Четырехзначное число загаданно! Ваш ход:");
+    function check_bulls(n1, n2 = rnd_num) {
+        var result = [];
+        n1 += ""; n2 += "";
+        for(var i = 0; i < n1.length; i++) 
+            if(n2.indexOf(n1[i]) == i) result.push(n1[i]);
+        return result;
+    }
+
+    while (!end_game) {
+        rnd_num = generate_number();
+        count_steps = 0;
+        n = prompt("Четырехзначное число загаданно! Ваш ход:");
         
-    //     while (!end_game) {
-    //         count++;
-    //         switch (true) {
-    //             case n === null:
-    //                 alert("Игра прекращена.");
-    //                 end_game = true;
-    //                 break;
-    //             case n == rnd_num:
-    //                 alert("Молодцом! Вы угадали! Этот число " + rnd_num + ". Количество ходов " + count + ".");
-    //                 end_game = true;
-    //                 break;
-    //             case rnd_num < n:
-    //                 n = prompt("Загаданное число меньше " + n + "! Ваш ход:");
-    //                 break;
-    //             case rnd_num > n: 
-    //                 n = prompt("Загаданное число больше " + n + "! Ваш ход:");
-    //                 break;
-    //             default: break;
-    //         }
-    //         //if(end_game) break;
-    //     }
+        while (!end_game) {
+            count_steps++;
+            switch (true) {
+                case n === null:
+                    alert("Игра прекращена.");
+                    end_game = true;
+                    break;
+                case n == rnd_num:
+                    alert("Молодцом! Вы угадали! Этот число " + rnd_num + ". Количество ходов " + count_steps + ".");
+                    end_game = true;
+                    break;
+                case rnd_num < n:
+                    n = prompt("Загаданное число меньше " + n + ". Коровы > " + check_cows(n) + ". Быки > " + check_bulls(n) + ". Ваш ход:");
+                    break;
+                case rnd_num > n: 
+                    n = prompt("Загаданное число больше " + n + ". Коровы > " + check_cows(n) + ". Быки > " + check_bulls(n) + ". Ваш ход:");
+                    break;
+                default: break;
+            }
+            //if(end_game) break;
+        }
 
-    //     if(confirm("Сыграем еще?")) end_game = false;
-    // }
+        if(confirm("Сыграем еще?")) end_game = false;
+    }
 };
